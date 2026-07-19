87250a72700504642db70bb01f9df567ef0da3f3
Fakhria Rasikh <fakhriarasikh@gmail.com>
2026-07-18 22:31:50 -0400

    Add sum13 function to calculate sum excluding 13

---
+++ codingbat/list-2/sum13.py
@@ -0,0 +1,9 @@
+def sum13(nums):
+  total = 0
+  for i in range(len(nums)):
+    if nums[i] == 13:
+      continue
+    if i > 0 and nums[i-1] == 13:
+      continue
+    total += nums[i]
+  return total
