0ab5f5a89d5f376751ecef37793e613ca8c279ba
Fakhria Rasikh <fakhriarasikh@gmail.com>
2026-07-18 22:15:39 -0400

    Add cat_dog function to count 'cat' and 'dog'

---
+++ codingbat/string-2/cat-dog.py
@@ -0,0 +1,9 @@
+def cat_dog(str):
+  count_cat = 0
+  count_dog = 0
+  for i in range(len(str)-1):
+    if str[i:i+3] == "cat":
+      count_cat += 1
+    if str[i:i+3] == "dog":
+      count_dog += 1
+  return (count_dog == count_cat)
