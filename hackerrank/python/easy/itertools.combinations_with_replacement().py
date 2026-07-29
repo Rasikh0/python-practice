21322384a5bf2aa4e4ff0ec33817680feb219dcb
Fara Rasikh <fakhriarasikh@gmail.com>
2026-07-29 14:53:43 -0400

    Add combinations_with_replacement implementation

---
+++ hackerrank/python/easy/itertools.combinations_with_replacement().py
@@ -0,0 +1,7 @@
+from itertools import combinations_with_replacement
+
+string, k = input().split()
+k = int(k)
+
+for item in list(combinations_with_replacement(sorted(string), k)):
+    print(''.join(item))
