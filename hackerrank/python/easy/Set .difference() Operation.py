45f0da300850c3e964966fa113cf2bef0992a6e6
Fara Rasikh <fakhriarasikh@gmail.com>
2026-07-30 12:51:45 -0400

    Add Set.difference() operation for subscriber counts

---
+++ hackerrank/python/easy/Set .difference() Operation.py
@@ -0,0 +1,8 @@
+students_eng = int(input())
+subscribers_eng = set(map(int, input().split()))
+students_fren = int(input())
+subscribers_fren = set(map(int, input().split()))
+
+answer = subscribers_eng.difference(subscribers_fren)
+
+print(len(answer))
