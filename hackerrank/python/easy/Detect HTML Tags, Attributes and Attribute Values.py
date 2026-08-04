5e0d2f1ca367bc05f2eb5c6ef065cbf8e35cab05
Fara Rasikh <fakhriarasikh@gmail.com>
2026-08-04 17:59:03 -0400

    Add HTML parser to detect tags and attributes

---
+++ hackerrank/python/easy/Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,11 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        for attr in attrs:
+            print("-> " + attr[0] + " > " + attr[1])
+
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
