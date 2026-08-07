f928d147aef050d840d6a8c3eb008e2b5d50ffa6
Fakhriya Rasikh <fakhriarasikh@gmail.com>
2026-08-07 18:15:11 -0400

    Add Minion Game implementation in Python

---
+++ hackerrank/python/medium/The Minion Game.py
@@ -0,0 +1,23 @@
+def minion_game(string):
+    vowels = "AEIOU"
+    kevin_scores = 0
+    stuart_scores = 0
+    length = len(string)
+    
+    for i in range(length):
+        if string[i] in vowels:
+            kevin_scores += length - i
+        else:
+            stuart_scores += length - i
+    
+    if stuart_scores > kevin_scores:
+        print(f"Stuart {stuart_scores}")
+    elif kevin_scores > stuart_scores:
+        print(f"Kevin {kevin_scores}") 
+    else:
+        print("Draw")
+            
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
