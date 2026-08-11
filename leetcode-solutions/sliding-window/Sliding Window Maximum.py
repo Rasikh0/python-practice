64095f2741f593ede621379ea82c88428e7712e1
Fakhriya Rasikh <fakhriarasikh@gmail.com>
2026-08-10 22:14:25 -0400

    Fix sliding window maximum output variable name

--- leetcode-solutions/sliding-window/Sliding Window Maximum.py
+++ leetcode-solutions/sliding-window/Sliding Window Maximum.py
@@ -12,8 +12,8 @@ class Solution:
             if l > q[0]:
                 q.popleft()
             
-            if (r + l) >= k:
-                out.append(nums[q[0]])
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
                 l += 1
             r += 1
         return output
