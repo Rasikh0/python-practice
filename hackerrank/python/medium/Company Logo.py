2768bdccf3f8316334291abb19957508722ba03d
Fakhriya Rasikh <fakhriarasikh@gmail.com>
2026-08-13 20:40:30 -0400

    Add solution for Company Logo problem
    Implement Company Logo problem solution to count letter frequencies and print the top three.

---
+++ hackerrank/python/medium/Company Logo.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    s = input()
+    s = sorted(s)
+    
+    frequency = {}
+    
+    for letter in s:
+        if letter in frequency:
+            frequency[letter] += 1
+        else:
+            frequency[letter] = 1
+    
+    frequency_sorted = sorted(frequency.items(), key=lambda x:(-x[1], x[0]))
+    for k, v in frequency_sorted[:3]:
+        print(k, v)
